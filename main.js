027fc5c849225876b290041e83a9916bfd843b15
Kerlan London <kerlanlondon@gmail.com>
2022-09-10 11:02:58 -0400

    Pass The Message

--- main.js
+++ main.js
@@ -1,45 +1,17 @@
-//immediate invoked function expression
 (function() {
-    const quotes = [
-      {
-        quote:
-          "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
-        author: " Life"
-      },
-      {
-        quote:
-          "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
-        author: "John Kenneth Galbraith"
-      },
-      {
-        quote:
-          "God save me from my friends. I can protect myself from my enemies.",
-        author: "Claude Louis Hector de Villars "
-      },
-      {
-        quote: "The price of anything is the amount of life you exchange for it.",
-        author: "David Thoreau"
-      },
-      {
-        quote:
-          "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
-        author: "Charles Lindbergh"
-      },
-      {
-        quote:
-          "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
-        author: " Tyne Daly"
-      }
-    ];
-  ////////////////////////////////////////
+  document.getElementById("btn").addEventListener("click", function(event){
+    let inputVal = document.getElementById("int-message").value 
+    if (Boolean(inputVal) === false ) {
+      document.getElementById("error").style.display = "inline-block"
+      setTimeout(()=>{
+        document.getElementById("error").style.display = "none"
+      }, 2000)
+    } else {
+      document.getElementById("message").innerText = inputVal
+      document.getElementById("int-message").value = ""
 
-  document.getElementById("button").addEventListener("click", function(){
-    const ranNum = Math.floor(Math.random() * quotes.length)
-    document.getElementById("para").innerText = quotes[ranNum].quote
-    document.getElementById("author-name").innerText = quotes[ranNum].author
-    //console.log(document.getElementById("para").innerText)
-    //console.log(quotes[ranNum].quote)
+    }
+    
+    //console.log(document.getElementById("int-message").value)
   })
-
-  })();
-  
+})()

267639ef3faed0b44b53797fade6a7353b8b339c
Kerlan London <kerlanlondon@gmail.com>
2022-09-10 19:06:17 -0400

    slider

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-(function () {
-  const colors = ["blue", "green", "orange", "yellow", "pink", "aquamareen", "black", "brown", "grey" , "red"]
-  document.getElementById("btn-main").addEventListener("click", function(){
-    let x = Math.floor(Math.random() * colors.length)
-    document.getElementById("background-container").style.backgroundColor = colors[x]
+(function(){
+  const imgArr = ["https://js-beginners.github.io/background-image-slider-project/img/contBcg-0.jpeg","https://js-beginners.github.io/background-image-slider-project/img/contBcg-1.jpeg","https://js-beginners.github.io/background-image-slider-project/img/contBcg-2.jpeg","https://js-beginners.github.io/background-image-slider-project/img/contBcg-3.jpeg","https://js-beginners.github.io/background-image-slider-project/img/contBcg-4.jpeg"]
+  let countPlus = 0
+  document.getElementById("btn-l").addEventListener("click", function(){
+    countPlus++
+    if(countPlus < imgArr.length){
+      document.getElementById("image-slider").style.backgroundImage = `url(${imgArr[countPlus]})`
+    } else {
+      countPlus = 0
+      document.getElementById("image-slider").style.backgroundImage = `url(${imgArr[countPlus]})`
+
+    }
+  })
+  /////////////
+  document.getElementById("btn-r").addEventListener("click", function(){
+    countPlus--
+    if(countPlus < 0){
+      countPlus = imgArr.length -1
+      document.getElementById("image-slider").style.backgroundImage = `url(${imgArr[countPlus]})`
+    } else {
+      document.getElementById("image-slider").style.backgroundImage = `url(${imgArr[countPlus]})`
+    }
+    
   })
 })()

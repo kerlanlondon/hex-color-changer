6a5982d8e166f10e1d96f3f2af086b71c957a975
Kerlan London <kerlanlondon@gmail.com>
2022-09-09 23:18:55 -0400

    Random Quote Generator

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
-const arrColors = ["0","1","2","3","4","5","6","7","8","9","A","B","C", "D", "E", "F"]
+//immediate invoked function expression
+(function() {
+    const quotes = [
+      {
+        quote:
+          "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+        author: " Life"
+      },
+      {
+        quote:
+          "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+        author: "John Kenneth Galbraith"
+      },
+      {
+        quote:
+          "God save me from my friends. I can protect myself from my enemies.",
+        author: "Claude Louis Hector de Villars "
+      },
+      {
+        quote: "The price of anything is the amount of life you exchange for it.",
+        author: "David Thoreau"
+      },
+      {
+        quote:
+          "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+        author: "Charles Lindbergh"
+      },
+      {
+        quote:
+          "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+        author: " Tyne Daly"
+      }
+    ];
+  ////////////////////////////////////////
 
-document.getElementById("button-id").addEventListener("click", function(){
-    const arrHexNum = []
-    let num = arrColors.length
-    for (let i = 0; i < 6; i++) {
-        let ranNum = Math.floor(Math.random() * num)
-        arrHexNum.push(arrColors[ranNum])
-    }
-    const newHex = arrHexNum.reduce((before,after) => before += after)
-    document.getElementById("main-background").style.backgroundColor = `#${newHex}`
-    console.log(`#${newHex}`)
-})
+  document.getElementById("button").addEventListener("click", function(){
+    const ranNum = Math.floor(Math.random() * quotes.length)
+    document.getElementById("para").innerText = quotes[ranNum].quote
+    document.getElementById("author-name").innerText = quotes[ranNum].author
+    //console.log(document.getElementById("para").innerText)
+    //console.log(quotes[ranNum].quote)
+  })
+
+  })();
+  
